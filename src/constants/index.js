51c96305ec16972845967c19de5317577bc4b83d
Swastik Biswas <swastikbiswas962@gmail.com>
2023-03-16 16:34:05 +0530

    edited from template

---
+++ src/constants/index.js
@@ -0,0 +1,197 @@
+import { people01, people02, people03, facebook, instagram, linkedin, twitter, airbnb, binance, coinbase, dropbox, send, shield, star  } from "../assets";
+
+export const navLinks = [
+  {
+    id: "home",
+    title: "Home",
+  },
+  {
+    id: "features",
+    title: "Features",
+  },
+  {
+    id: "product",
+    title: "Product",
+  },
+  {
+    id: "clients",
+    title: "Clients",
+  },
+];
+
+export const features = [
+  {
+    id: "feature-1",
+    icon: star,
+    title: "Analyze Trends",
+    content:
+      "Targeted practice papers with analysis and feedback improve performance and help students stay ahead of competition.",
+  },
+  {
+    id: "feature-2",
+    icon: shield,
+    title: "Self Assessment",
+    content:
+      "Targeted practice papers with analysis and feedback improve student performance in competitive exams, enhancing career potential.",
+  },
+  {
+    id: "feature-3",
+    icon: send,
+    title: "Career Growth",
+    content:
+      "Our service provides individualized career counseling and analysis to enhance student performance and achieve academic goals.",
+  },
+];
+
+export const feedback = [
+  {
+    id: "feedback-1",
+    content:
+      "Money is only a tool. It will take you wherever you wish, but it will not replace you as the driver.",
+    name: "Herman Jensen",
+    title: "Founder & Leader",
+    img: people01,
+  },
+  {
+    id: "feedback-2",
+    content:
+      "Money makes your life easier. If you're lucky to have it, you're lucky.",
+    name: "Steve Mark",
+    title: "Founder & Leader",
+    img: people02,
+  },
+  {
+    id: "feedback-3",
+    content:
+      "It is usually people in the money business, finance, and international trade that are really rich.",
+    name: "Kenn Gallagher",
+    title: "Founder & Leader",
+    img: people03,
+  },
+];
+
+export const stats = [
+  {
+    id: "stats-1",
+    title: "Exam Papers",
+    value: "250+",
+  },
+  {
+    id: "stats-2",
+    title: "Trusted by Companies",
+    value: "7+",
+  },
+  {
+    id: "stats-3",
+    title: "Projected Funding",
+    value: "$15M+",
+  },
+];
+
+export const footerLinks = [
+  {
+    title: "Useful Links",
+    links: [
+      {
+        name: "Content",
+        link: "https://versavo-ai.vercel.app/content/",
+      },
+      {
+        name: "How it Works",
+        link: "https://versavo-ai.vercel.app/how-it-works/",
+      },
+      {
+        name: "Create",
+        link: "https://versavo-ai.vercel.app/create/",
+      },
+      {
+        name: "Explore",
+        link: "https://versavo-ai.vercel.app/explore/",
+      },
+      {
+        name: "Terms & Services",
+        link: "https://versavo-ai.vercel.app/terms-and-services/",
+      },
+    ],
+  },
+  {
+    title: "Community",
+    links: [
+      {
+        name: "Help Center",
+        link: "https://versavo-ai.vercel.app/help-center/",
+      },
+      {
+        name: "Partners",
+        link: "https://versavo-ai.vercel.app/partners/",
+      },
+      {
+        name: "Suggestions",
+        link: "https://versavo-ai.vercel.app/suggestions/",
+      },
+      {
+        name: "Blog",
+        link: "https://versavo-ai.vercel.app/blog/",
+      },
+      {
+        name: "Newsletters",
+        link: "https://versavo-ai.vercel.app/newsletters/",
+      },
+    ],
+  },
+  {
+    title: "Partner",
+    links: [
+      {
+        name: "Our Partner",
+        link: "https://versavo-ai.vercel.app/our-partner/",
+      },
+      {
+        name: "Become a Partner",
+        link: "https://versavo-ai.vercel.app/become-a-partner/",
+      },
+    ],
+  },
+];
+
+export const socialMedia = [
+  {
+    id: "social-media-1",
+    icon: instagram,
+    link: "https://www.instagram.com/",
+  },
+  {
+    id: "social-media-2",
+    icon: facebook,
+    link: "https://www.facebook.com/",
+  },
+  {
+    id: "social-media-3",
+    icon: twitter,
+    link: "https://www.twitter.com/",
+  },
+  {
+    id: "social-media-4",
+    icon: linkedin,
+    link: "https://www.linkedin.com/",
+  },
+];
+
+export const clients = [
+  {
+    id: "client-1",
+    logo: airbnb,
+  },
+  {
+    id: "client-2",
+    logo: binance,
+  },
+  {
+    id: "client-3",
+    logo: coinbase,
+  },
+  {
+    id: "client-4",
+    logo: dropbox,
+  },
+];
